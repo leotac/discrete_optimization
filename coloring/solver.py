56d8259b8ee4ef932442ed16d52fe03ee6d87af8
Leonardo Taccari <floor51@gmail.com>
2013-08-04 11:52:58 +0200

    Use SCIP and print optimal value in output file

--- coloring/solver.py
+++ coloring/solver.py
@@ -4,6 +4,51 @@
 import os
 from subprocess import Popen, PIPE
 
+def cplex(nodeCount, edgeCount, edges):
+    data = open("coloring.dat","w")
+    data.write("data;\n")
+    data.write("param N := " + str(nodeCount) + ";\n")
+    data.write("param M := " + str(edgeCount) + ";\n")
+
+    data.write("set E :=\n")
+    for e in edges:
+       data.write("("+str(e[0])+","+str(e[1])+")\n")
+    data.write(";\n")
+    data.close()
+
+    process = Popen(['ampl', 'coloring.run'])
+    #(stdout, stderr) = process.communicate()
+    process.wait()
+    return 
+
+
+def scip(nodeCount, edgeCount, edges):
+    data = open("coloring.col","w")
+    data.write("p edge " + str(nodeCount) + " " + str(edgeCount) + "\n")
+
+    for e in edges:
+       data.write("e "+str(e[0]+1)+" "+str(e[1]+1)+"\n")
+    data.write("\n")
+    data.close()
+
+    #process = Popen(['/opt/scipoptsuite/scip-3.0.1/examples/Coloring/bin/coloring', '-f', 'coloring.col'])
+    process = Popen(['/opt/scipoptsuite/scip-3.0.1/examples/Coloring/bin/coloring', '-c', 'read coloring.col', '-c', 'set limits time 20','-c', 'optimize', '-c','write problem coloring.csol','-c','quit'])
+    #(stdout, stderr) = process.communicate()
+    process.wait()
+    
+    csol_solution =  open("coloring.csol","r")
+    opt = csol_solution.readline().split()[1]
+    sol = csol_solution.readline()
+    csol_solution.close()
+
+    out_solution = open("coloring.out","w")
+    out_solution.write(opt + "\n")
+    out_solution.write(sol)
+    out_solution.close()
+
+    return 
+
+
 def solveIt(inputData):
     # Modify this code to run your optimization algorithm
 
@@ -19,25 +64,16 @@ def solveIt(inputData):
         line = lines[i]
         parts = line.split()
         edges.append((int(parts[0]), int(parts[1])))
-
-    data = open("coloring.dat","w")
-    data.write("data;\n")
-    data.write("param N := " + str(nodeCount) + ";\n")
-    data.write("param M := " + str(edgeCount) + ";\n")
-
-    data.write("set E :=\n")
-    for e in edges:
-       data.write("("+str(e[0])+","+str(e[1])+")\n")
-    data.write(";\n")
-    data.close()
-
-    process = Popen(['ampl', 'coloring.run'],stdout=PIPE)
-    (stdout, stderr) = process.communicate()
+   
+    #cplex(nodeCount, edgeCount, edges)
+    scip(nodeCount, edgeCount, edges)
 
     output = open("coloring.out","r")
+    opt = output.readline()
+    opt = opt.strip()
     solution = output.read()
     # prepare the solution in the specified output format
-    outputData = str(nodeCount) + ' ' + str(0) + '\n'
+    outputData = opt + ' ' + str(1) + '\n'
     outputData += solution
     #outputData += ' '.join(map(str, solution))
 
@@ -55,4 +91,6 @@ if __name__ == '__main__':
         print solveIt(inputData)
     else:
         print 'This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/gc_4_1)'
+
+
 
